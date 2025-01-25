3227c7c1617d91d70dbeb1ba719c28eaf094e287
Ivan Adamo <ivan.adamo.8@gmail.com>
2025-01-25 11:36:25 -0300

    if

---
+++ 12.if.js
@@ -0,0 +1,9 @@
+let nombre = "Sergio";
+
+if (nombre === "Ivan") {
+  console.log("Hola " + nombre);
+} else if (nombre === "Sergio") {
+  console.log("Hola " + nombre);
+} else {
+  console.log("Nombre no encontrado");
+}
